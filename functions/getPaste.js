595210cd962ec943f2f7733af115df22b732e770
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-03-29 02:36:19 +0600

    Update getPaste.js

--- functions/getPaste.js
+++ functions/getPaste.js
@@ -1,4 +1,4 @@
-export async function onRequestGet() {
+export async function onRequestGet(c) {
   return new Response(await variables.get('paste'), {
     headers: { 'Content-Type': 'text/plain' },
   });
