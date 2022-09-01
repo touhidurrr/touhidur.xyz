8a60f9110e0905966035edc2a233d49b4cc49091
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-09-02 01:19:20 +0600

    Create testPost.js

---
+++ functions/testPost.js
@@ -0,0 +1,8 @@
+export async function onRequestPost({ env, request }) {
+  const body = await request.text();
+  if (body.startsWith('{')) {
+    body = JSON.stringify(JSON.parse(body), null, 2);
+  }
+  await env.variables.put('testPost', body);
+  return new Response('OK');
+}
