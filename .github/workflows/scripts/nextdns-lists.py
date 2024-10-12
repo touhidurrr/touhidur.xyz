df05d863368776aa667e82e51dd178bb7dfc482d
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-10-12 23:45:18 +0600

    Update nextdns-lists.py

--- .github/workflows/scripts/nextdns-lists.py
+++ .github/workflows/scripts/nextdns-lists.py
@@ -62,7 +62,7 @@ hostsList.sort()
 domainsList.sort()
 
 # Starting lines to specify various information about Blocklist
-now = datetime.datetime.now(datetime.UTC)
+now = datetime.now(datetime.UTC)
 info = '''\
 # --------------------------------------------------------------------------------------------------------
 # Title: NextDNS Ads & Trackers Blocklist Mirror by touhidurrr
